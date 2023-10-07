8432b9b5bc6315bcc2d553771d5a382085490c9f
Fatiunis <123609641+Fatiunis@users.noreply.github.com>
2023-10-07 16:37:01 -0600

    A

---
+++ 2nda entrega TotusTuus/2nda entrega TotusTuus/src/Cliente.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Maria Cospin
+ */
+public class Cliente {
+  private String nombre;
+  private String Nit;
+  private String direccion;
+
+    public Cliente() {
+        this.nombre = "";
+        this.Nit = "";
+        this.direccion = "";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNit() {
+        return Nit;
+    }
+
+    public void setNit(String Nit) {
+        this.Nit = Nit;
+    }
+
+ 
+}
